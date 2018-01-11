88681811ec2ba27b53972208eb0f4924f814c086
Louis Filip <louisfilip@gmail.com>
2018-01-11 10:25:32 -0600

    Updated to add reset Button

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var cellWidth = 0;
 var cellHeight = 0;
 var numberOfCells = 0;
 var gridContainer;
+var resetButton;
 
 function visitCell(e) {
   console.log("visited");
@@ -10,23 +11,32 @@ function visitCell(e) {
 }
 
 function createGrid( pixelDensity ){
+  resetButton = document.querySelector("button");
   cellWidth = 100 / pixelDensity;
   cellHeight = 100 / pixelDensity;
   numberOfCells = pixelDensity * pixelDensity;
   console.log(cellHeight);
   gridContainer = document.querySelector("#grid");
   console.log(gridContainer);
+  resetButton.addEventListener("click", emptyGrid);
 
   for(i = 0;i<numberOfCells;i++){
     let cell = document.createElement('div');
+    cell.classList.add("cell");
     cell.classList.add("cell-unvisited");
     cell.setAttribute("id", "myBtn");
     cell.style.height = cellHeight + "%";
     cell.style.width = cellWidth + "%";
-    cell.addEventListener("click",visitCell);
     cell.addEventListener("mouseover", visitCell);
+
     cell.setAttribute('width', "30%");
     cell.setAttribute("height", "30%");
     gridContainer.appendChild(cell);
   }
+}
+
+function emptyGrid(){
+  gridContainer.innerHTML = "";
+   let newPixelDensity = window.prompt("Enter the size ( 1 - 500 )", "16");
+   createGrid(newPixelDensity);
 }
